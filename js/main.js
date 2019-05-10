d7a88f913d106594ff130f67c0b82182fcdd8122
JON PACKLES <JON.PACKLES@GMAIL.COM>
2019-05-10 02:36:30 -0400

    ok

--- js/main.js
+++ js/main.js
@@ -1,12 +1,115 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function() {
+
+	$(document).on('scroll resize', function() {
+     
+     var $d = $(document),
+         $w = $(window);
+     
+     $('#scroll-bar').width(
+       ($d.scrollTop() / ($d.height() - $w.height()) * $d.height()) + 'px'
+     );
+
+     if ($(window).scrollTop() > 200){
+      
+        $('.mainTitle').addClass( "mainTitleOutline");
+    }
+    else {
+        $('.mainTitle').removeClass("mainTitleOutline");
+    }
 
 
-	$('#eye').click(function(){
-		console.log("you clicked the eye");
-		$('#trackerP').slideToggle('fast');
-	});
+     
+   });
+
+
 });
 
 
 
+// When the user scrolls the page, execute myFunction 
+
+
+// Get the header
+var header = document.getElementById("topStuff");
+
+// Get the offset position of the navbar
+
+
+
+// $(function() {
+  
+//    $(document).on('scroll resize', function() {
+     
+//      var $d = $(document),
+//          $w = $(window);
+     
+//      $('div#scroll-bar').width(
+//        ($d.scrollTop() / ($d.height() - $w.height()) * $d.height()) + 'px'
+//      );
+     
+//    });
+  
+
+// });
+
+
+
+$(document).ready(function() {
+
+    var getMax = function() {
+        return $(document).height() - $(window).height();
+    }
+
+    var getValue = function() {
+        return $(window).scrollTop();
+    }
+
+    if ('max' in document.createElement('progress')) {
+        var progressBar = $('progress');
+        
+        progressBar.attr({
+            max: getMax()
+        });
+
+        $(document).on('scroll', function() {
+            progressBar.attr({
+                value: getValue()
+            });
+        });
+
+        $(window).resize(function() {
+            
+            progressBar.attr({
+                max: getMax(),
+                value: getValue()
+            });
+        });
+
+    } else {
+
+        var progressBar = $('.progress-bar'),
+            max = getMax(),
+            value, width;
+
+        var getWidth = function() {
+            
+            value = getValue();
+            width = (value / max) * 100;
+            width = width + '%';
+            return width;
+        }
+
+        var setWidth = function() {
+            progressBar.css({
+                width: getWidth()
+            });
+        }
+
+        $(document).on('scroll', setWidth);
+        $(window).on('resize', function() {
+            
+            max = getMax();
+            setWidth();
+        });
+    }
+});
